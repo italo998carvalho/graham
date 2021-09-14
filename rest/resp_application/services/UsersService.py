f650f5e7df2acb3d52dca454c3d9622e6993261b
italo998carvalho <italo.998.carvalho@gmail.com>
2021-09-14 19:08:22 -0300

    Fix receiving payload

--- rest/resp_application/services/UsersService.py
+++ rest/resp_application/services/UsersService.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+import json
 from models.User import User
 
 class UsersService:
@@ -6,18 +6,19 @@ class UsersService:
     self.db = db
 
   def save(self, user_request):
+    req = json.loads(user_request)
     user = User(
-      name = user_request['name'],
-      email = user_request['email']
+      req['name'],
+      req['email']
     )
 
     try:
       self.db.session.add(user)
       self.db.session.commit()
 
-      return jsonify({'code': 201, 'body': {'message': 'User succesfully created!'}}), 201
+      return json.dumps({'code': 201, 'body': {'message': 'User succesfully created!'}}), 201
     except Exception as e:
-      return jsonify({'code': 500, 'body': {'message': e}}), 500
+      return json.dumps({'code': 500, 'body': {'message': e}}), 500
 
   def listAll(self):
     users = User.query.all()
@@ -30,6 +31,6 @@ class UsersService:
           'email': u.email
         })
         
-      return jsonify({'code': 200, 'body': {'message': resp_payload}}), 200
+      return json.dumps({'code': 200, 'body': {'message': resp_payload}}), 200
     else:
-      return jsonify({'code': 404, 'body': {'message': 'User not found!'}}), 404
+      return json.dumps({'code': 404, 'body': {'message': 'User not found!'}}), 404
