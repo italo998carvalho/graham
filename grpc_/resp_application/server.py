b4fdf10a416ef9543cd9e5228e4e0bcee50e5bc5
italo998carvalho <italo.998.carvalho@gmail.com>
2021-09-21 18:44:31 -0300

    Organize service in a file

--- grpc_/resp_application/server.py
+++ grpc_/resp_application/server.py
@@ -1,56 +1,18 @@
-import random, grpc
+import grpc
+from sqlalchemy import create_engine
 from concurrent import futures
 from database import Base
-from models.User import User
-from users.users_pb2 import (
-  SaveUserRequest,
-  UserResponse,
-  GetUsersRequest,
-  GetUsersResponse
-)
 import users.users_pb2_grpc as users_pb2_grpc
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from services.UsersService import UsersService
 
 engine = create_engine('postgresql://comm-tests:comm-tests@localhost:15432/comm-tests', echo=True)
-
-class UsersService(users_pb2_grpc.UsersServicer):
-  def __init__(self):
-    Session = sessionmaker(bind = engine)
-    self.session = Session()
-
-  def Save(self, request, context):
-    user = User(
-      name = request.name,
-      email = request.email
-    )
-
-    self.session.add(user)
-    self.session.commit()
-
-    return UserResponse(
-      id = user.id,
-      name = user.name,
-      email = user.email
-    )
-
-  def Get(self, request, context):
-    users_response = []
-    for user in self.session.query(User).order_by(User.id):
-      users_response.append(UserResponse(
-        id = user.id,
-        name = user.name,
-        email = user.email
-      ))
-    
-    return GetUsersResponse(users = users_response)
 
 def serve():
   print('Starting server...')
   Base.metadata.create_all(engine)
   server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
   users_pb2_grpc.add_UsersServicer_to_server(
-    UsersService(), server
+    UsersService(engine), server
   )
   server.add_insecure_port("[::]:50051")
   print(f'Started on port {50051}')
---
+++ grpc_/resp_application/services/UsersService.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import sessionmaker
+from models.User import User
+from users.users_pb2 import (UserResponse, GetUsersResponse)
+import users.users_pb2_grpc as users_pb2_grpc
+
+class UsersService(users_pb2_grpc.UsersServicer):
+  def __init__(self, engine):
+    Session = sessionmaker(bind = engine)
+    self.session = Session()
+
+  def Save(self, request, context):
+    user = User(
+      name = request.name,
+      email = request.email
+    )
+
+    self.session.add(user)
+    self.session.commit()
+
+    return UserResponse(
+      id = user.id,
+      name = user.name,
+      email = user.email
+    )
+
+  def Get(self, request, context):
+    users_response = []
+    for user in self.session.query(User).order_by(User.id):
+      users_response.append(UserResponse(
+        id = user.id,
+        name = user.name,
+        email = user.email
+      ))
+    
+    return GetUsersResponse(users = users_response)
