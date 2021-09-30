3f59ab7137149d6a9082ba2d11c08e876412bbc4
italo998carvalho <italo.998.carvalho@gmail.com>
2021-09-30 19:15:57 -0300

    Remove older files

--- request_app/setup.py
+++
@@ -1,5 +0,0 @@
-from services.RestService import RestService
-from services.GrpcService import GrpcService
-
-restService = RestService('http://localhost:5001')
-grpcService = GrpcService('localhost:50051')
