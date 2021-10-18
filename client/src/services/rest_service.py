dba2c3a2a03670eeb4bb3c2ec1bdd087587597b0
italo998carvalho <italo.998.carvalho@gmail.com>
2021-10-18 18:27:35 -0300

    Solve lint issues in rest_service.py

--- client/src/services/rest_service.py
+++ client/src/services/rest_service.py
@@ -1,18 +1,21 @@
-import requests, json, logging
+import requests
+import json
+import logging
+
 
 class RestService:
-  def __init__(self, host):
-    self.host = host
+    def __init__(self, host):
+        self.host = host
 
-  def save(self, form):
-    payload = {}
-    payload['name'] = form['name']
-    payload['email'] = form['email']
-    return requests.post(f"{self.host}/user", json = json.dumps(payload))
+    def save(self, form):
+        payload = {}
+        payload['name'] = form['name']
+        payload['email'] = form['email']
+        return requests.post(f"{self.host}/user", json=json.dumps(payload))
 
-  def get(self):
-    try:
-      return requests.get(f"{self.host}/user").json()
-    except requests.exceptions.ConnectionError as e:
-      logging.error(f'Error trying to get users list: {str(e)}')
-      return {'body': {'message': []}}
+    def get(self):
+        try:
+            return requests.get(f"{self.host}/user").json()
+        except requests.exceptions.ConnectionError as e:
+            logging.error(f'Error trying to get users list: {str(e)}')
+            return {'body': {'message': []}}
