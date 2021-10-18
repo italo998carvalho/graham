5e96f92b5eaabdaf0d0f7d207c4f6e7f5df10faf
italo998carvalho <italo.998.carvalho@gmail.com>
2021-10-18 18:32:09 -0300

    Solve lint issues in grpc_service.py

--- client/src/services/grpc_service.py
+++ client/src/services/grpc_service.py
@@ -1,31 +1,34 @@
-import grpc, logging
+import grpc
+import logging
 from proto_users.users_pb2 import (
-  SaveUserRequest,
-  GetUsersRequest
+    SaveUserRequest,
+    GetUsersRequest
 )
 from proto_users.users_pb2_grpc import UsersStub
 
+
 class GrpcService:
-  def __init__(self, host):
-    channel = grpc.insecure_channel(host)
-    self.client = UsersStub(channel)
+    def __init__(self, host):
+        channel = grpc.insecure_channel(host)
+        self.client = UsersStub(channel)
 
-  def save(self, form):
-    request = SaveUserRequest(
-      name = form['name'],
-      email = form['email']
-    )
+    def save(self, form):
+        request = SaveUserRequest(
+            name=form['name'],
+            email=form['email']
+        )
 
-    return self.client.Save(request)
+        return self.client.Save(request)
 
-  def get(self):
-    request = GetUsersRequest()
-    try:
-      return self.client.Get(request)
-    except grpc._channel._InactiveRpcError as e:
-      logging.error(f'Error trying to get users list: {str(e)}')
-      class Object:
-        def __init__(self):
-          self.users = []
+    def get(self):
+        request = GetUsersRequest()
+        try:
+            return self.client.Get(request)
+        except grpc._channel._InactiveRpcError as e:
+            logging.error(f'Error trying to get users list: {str(e)}')
 
-      return Object()
+            class Object:
+                def __init__(self):
+                    self.users = []
+
+            return Object()
