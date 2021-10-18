5432a5715b01915c17977c9ea4fe38128b195102
italo998carvalho <italo.998.carvalho@gmail.com>
2021-10-18 18:39:58 -0300

    Solve lint issues in rest_resvice_test.py

--- client/tests/services/rest_service_test.py
+++ client/tests/services/rest_service_test.py
@@ -1,30 +1,37 @@
-import requests, json
+import requests
+import json
 from unittest.mock import MagicMock
 from src.services.rest_service import RestService
 
+
 def test_post_request_is_being_called_with_proper_parameters():
-	requests.post = MagicMock()
+    requests.post = MagicMock()
 
-	service = RestService('http://tmp.com')
-	payload = {'name': 'raven', 'email': 'reyes@ark.com'}
-	service.save(payload)
+    service = RestService('http://tmp.com')
+    payload = {'name': 'raven', 'email': 'reyes@ark.com'}
+    service.save(payload)
 
-	requests.post.assert_called_with('http://tmp.com/user', json=json.dumps(payload))
+    requests.post.assert_called_with(
+        'http://tmp.com/user',
+        json=json.dumps(payload)
+    )
+
 
 def test_get_request_is_being_called_with_proper_parameters():
-	requests.get = MagicMock()
+    requests.get = MagicMock()
 
-	service = RestService('http://tmp.com')
-	service.get()
+    service = RestService('http://tmp.com')
+    service.get()
 
-	requests.get.assert_called_with('http://tmp.com/user')
+    requests.get.assert_called_with('http://tmp.com/user')
+
 
 def test_get_works_even_with_connection_problems():
-	requests.get = MagicMock()
-	requests.get.side_effect = requests.exceptions.ConnectionError()
+    requests.get = MagicMock()
+    requests.get.side_effect = requests.exceptions.ConnectionError()
 
-	service = RestService('http://tmp.com')
-	result = service.get()
+    service = RestService('http://tmp.com')
+    result = service.get()
 
-	requests.get.assert_called_with('http://tmp.com/user')
-	assert result == {'body': {'message': []}}
+    requests.get.assert_called_with('http://tmp.com/user')
+    assert result == {'body': {'message': []}}
