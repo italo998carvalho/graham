a123dccc57a63fb00735d06d0ea8bc315ebb5d4b
italo998carvalho <italo.998.carvalho@gmail.com>
2021-10-19 19:01:39 -0300

    [REST] Solve lint issues in UsersView.py

--- servers/rest/views/UsersView.py
+++ servers/rest/views/UsersView.py
@@ -1,16 +1,17 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from services.UsersService import UsersService
 from database import db
 
 user = Blueprint('user', __name__)
 service = UsersService(db)
 
+
 @user.route('/user', methods=['POST', 'GET'])
 def users():
-  if request.method == 'POST':
-    req_payload = request.get_json()
+    if request.method == 'POST':
+        req_payload = request.get_json()
 
-    return service.save(req_payload)
-  
-  else:
-    return service.listAll()
+        return service.save(req_payload)
+
+    else:
+        return service.listAll()
