bf98930014c21e7107961f4516f84112e4da82e3
italo998carvalho <italo.998.carvalho@gmail.com>
2021-10-19 18:59:33 -0300

    [REST] Solve lint issues in app.py

--- servers/rest/app.py
+++ servers/rest/app.py
@@ -3,22 +3,27 @@ from database import db
 from config import PORT
 from views.UsersView import user
 
+db_config = 'postgresql://comm-tests:comm-tests@localhost:15432/comm-tests'
+
+
 def create_app(db):
-  app = Flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://comm-tests:comm-tests@localhost:15432/comm-tests'
-  app.secret_key = '123456789'
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_config
+    app.secret_key = '123456789'
 
-  db.init_app(app)
+    db.init_app(app)
 
-  app.register_blueprint(user)
+    app.register_blueprint(user)
 
-  return app
+    return app
+
 
 def setup_database(app):
-  with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
+
 
 if __name__ == '__main__':
-  app = create_app(db)
-  setup_database(app)
-  app.run(host='0.0.0.0', port=PORT, debug=True)
+    app = create_app(db)
+    setup_database(app)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
